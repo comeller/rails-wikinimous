bb1ea43271d4f8a571d8a39292ae922df654e047
come mellerio <come.mellerio@free.fr>
2018-03-06 18:38:23 +0100

    ex2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  new_article = Article.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph)
+end
